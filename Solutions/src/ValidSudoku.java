790145152f88d2f69b1fd955c04fe023ed94c2a6
George Gitonga <lawrenceisnt@gmail.com>
2024-01-12 16:38:46 +0300

    1122024

--- Solutions/src/ValidSudoku.java
+++ Solutions/src/ValidSudoku.java
@@ -4,15 +4,33 @@ public class ValidSudoku {
 
         int[][] board = {
             {7, 0, 2, 0, 5, 0, 6, 0, 0},
-            {0, 0, 0, 0, 0, 3 , 0, 0, 0},
+            {0, 0, 0, 0, 0, 3, 0, 0, 0},
             {1, 0, 0, 0, 0, 9, 5, 0, 0},
             {8, 0, 0, 0, 0, 0, 0, 9, 0},
             {0, 4, 3, 0, 0, 0, 7, 5, 0},
             {0, 9, 0, 0, 0, 0, 0, 0, 8},
             {0, 0, 9, 7, 0, 0, 0, 0, 5},
             {0, 0, 0, 2, 0, 0, 0, 0, 0},
-            {0, 0, 1, 0, 4, 0, 2, 0, 3}
+            {0, 0, 7, 0, 4, 0, 2, 0, 3}
             };
+
+        if(solveBoard(board)){
+            System.out.println("solved successfully");
+        }else{
+            System.out.println("Unsolvable board");
+        }
+
+        printBoard(board);
+
+    }
+
+    private static void printBoard(int[][] board) {
+        for(int row = 0; row < GRID_SIZE; row++){
+            for(int column = 0; column < GRID_SIZE; column++){
+                System.out.print(board[row][column]);
+            }
+            System.out.println();
+        }
     }
 
     private static boolean isNumberInRow(int[][] board, int number, int row){
@@ -55,9 +73,23 @@ public class ValidSudoku {
     private static boolean solveBoard(int[][] board){
         for(int row = 0; row < GRID_SIZE; row ++ ){
             for(int column = 0; column < GRID_SIZE; column++){
+                if(board[row][column] == 0){
+                    for (int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++){
+                        if(isValidPlacement(board, numberToTry, row, column)){
+                            board[row][column] = numberToTry;
 
+                            if (solveBoard(board)){
+                                return true;
+                            }else{
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
             }
         }
+        return true;
     }
 
 }
