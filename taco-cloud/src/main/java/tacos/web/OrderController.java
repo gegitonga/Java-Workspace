b746f16cb2ceafc34f446d7fd8997c665c36fa01
George Gitonga <lawrenceisnt@gmail.com>
2024-01-15 16:42:46 +0300

    validation

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,11 +1,16 @@
 package tacos.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import tacos.TacoOrder;
 
 @Slf4j
 @Controller
@@ -15,5 +20,18 @@ public class OrderController {
 	@GetMapping("/current")
 	public String orderForm() {
 		return "orderForm";
+	}
+	
+	@PostMapping
+	public String processOrder(@Valid TacoOrder order,Errors errors ,SessionStatus sessionStatus) {
+		
+		if(errors.hasErrors()) {
+			return "orderForm";
+		}
+		
+		log.info("Order submitted: {}", order);
+		sessionStatus.setComplete();
+		
+		return "redirect:/";
 	}
 }
