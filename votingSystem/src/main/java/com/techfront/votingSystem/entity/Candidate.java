2e2c72c072a363c009124b5e518bd62bb1066460
George Gitonga <lawrenceisnt@gmail.com>
2024-02-07 15:58:59 +0300

    vot

---
+++ votingSystem/src/main/java/com/techfront/votingSystem/entity/Candidate.java
@@ -0,0 +1,38 @@
+package com.techfront.votingSystem.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="candidates")
+public class Candidate {
+
+    public Candidate(Long id, String name){
+        this.id =id;
+        this.name =name;
+    }
+    @Id
+    @Column
+    private Long id;
+
+    @Column(name = "candidate_name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ votingSystem/src/main/java/com/techfront/votingSystem/entity/Citizen.java
@@ -0,0 +1,40 @@
+package com.techfront.votingSystem.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="citizens")
+public class Citizen {
+
+    public Citizen(Long id, String name){
+        super();
+        this.id = id;
+        this.name = name;
+    }
+    @Id
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="citizen_name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
