36b7ebe9075f4ddd931dcfad3c5d436ff6d5f1cd
George Gitonga <lawrenceisnt@gmail.com>
2024-02-15 08:48:01 +0300

    final

--- votingSystem/src/main/java/com/techfront/votingSystem/controller/VotingController.java
+++ votingSystem/src/main/java/com/techfront/votingSystem/controller/VotingController.java
@@ -4,18 +4,20 @@ import com.techfront.votingSystem.entity.Candidate;
 import com.techfront.votingSystem.entity.Citizen;
 import com.techfront.votingSystem.repositories.CandidateRepo;
 import com.techfront.votingSystem.repositories.CitizenRepo;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.jboss.logging.Logger;
 import java.util.List;
 
 @Controller
 public class VotingController {
 
+    public final Logger logger = Logger.getLogger(VotingController.class);
     @Autowired
     CitizenRepo citizenRepo;
 
@@ -24,12 +26,17 @@ public class VotingController {
 
     @RequestMapping("/")
     public String gotToVote(){
+        logger.info("Returning vote.html file");
         return "vote.html";
     }
 
     @RequestMapping("/dologin")
-    public String doLogin(@RequestParam String name, Model model){
+    public String doLogin(@RequestParam String name, Model model, HttpSession session){
+        logger.info("Getting citizen from database");
         Citizen citizen = citizenRepo.findByName(name);
+
+        session.setAttribute("citizen", citizen);
+
         if(!citizen.getHasvoted()) {
             List<Candidate> candidates = candidateRepo.findAll();
             model.addAttribute("candidates",candidates);
@@ -38,13 +45,19 @@ public class VotingController {
             return "/alreadyVoted.html";
         }
     }
-
     @RequestMapping("/voteFor")
-    public String voteFor(@RequestParam Integer id){
-        Candidate c = candidateRepo.findById((int)id);
-        c.setNumberOfVotes(c.getNumberOfVotes() + 1);
-        candidateRepo.save(c);
+    public String voteFor(@RequestParam Integer id, HttpSession session){
+        Citizen c = (Citizen) session.getAttribute("citizen");
 
-        return "voted.html";
+        if(!c.getHasvoted()) {
+            c.setHasvoted(true);
+
+            Candidate candidate = candidateRepo.findById((int) id);
+            candidate.setNumberOfVotes(candidate.getNumberOfVotes() + 1);
+            candidateRepo.save(candidate);
+            citizenRepo.save(c);
+            return "voted.html";
+        }
+        return "/alreadyVoted.html";
     }
 }
