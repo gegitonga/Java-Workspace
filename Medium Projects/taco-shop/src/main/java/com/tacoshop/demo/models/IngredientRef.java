08353d9a7ebb3101df5fd31f983b7aafe60e4543
Gitonga George <32628549+gegitonga@users.noreply.github.com>
2025-01-06 15:50:39 +0300

    pg

---
+++ Medium Projects/taco-shop/src/main/java/com/tacoshop/demo/models/IngredientRef.java
@@ -0,0 +1,12 @@
+package com.tacoshop.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class IngredientRef {
+	
+	private final String ingredient;
+
+}
