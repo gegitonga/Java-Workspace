5a862789fb1857d530e1c8a886eac3bcaa726f33
lawrence <lawrenceisnt@gmail.com>
2025-02-04 07:21:45 +0300

    Add initial implementation of CheckInclusion class with main method

---
+++ leetcode solutions/CheckInclusion.java
@@ -0,0 +1,12 @@
+public class CheckInclusion {
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        check(s1, s2);
+    }
+
+    public static boolean check(String s1, String s2) {
+        
+    }
+
+}
